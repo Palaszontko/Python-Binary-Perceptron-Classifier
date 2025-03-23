41d0728d65d68809bcf864419a70cb7fcec1bf46
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-03-23 14:05:14 +0100

    feat(data): add Perceptron class for handling training and testing data

--- src/main.py
+++ src/main.py
@@ -6,3 +6,11 @@ class Sample:
     def __str__(self):
         return f"Data Tag: {self.dataTag}, Weights: {self.weights}"
     
+class Perceptron:
+    def __init__(self, alpha : float, beta : float, trainDataPath : str, testDataPath : str):
+        self.alpha = alpha
+        self.beta = beta
+        self.threshold = random.randrange(-5,5)
+        self.trainDataPath = trainDataPath
+        self.testDataPath = testDataPath
+
